44bdead10eb7e85e0c5f43e2c63e573fb2140f3f
Vic-33 <balvivekreddy33@gmail.com>
2026-04-16 22:46:49 +0530

    setup.py and requirements.txt

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages,setup
+from typing import List
+
+MINUS_E_DOT = '-e .'
+
+def get_requirements(file_path:str)->List[str]:
+    "It will return list of requirements"
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n"," ") for req in requirements]
+
+        if MINUS_E_DOT in requirements:
+            requirements.remove(MINUS_E_DOT)
+    
+    return requirements
+
+
+setup (
+    name = "mlproject",
+    version = "0.0.1",
+    author = "K Balvivek Reddy",
+    author_email = "balvivekreddy33@gmail.com",
+    packages = find_packages(),
+    install_requires=get_requirements('requirements.txt')
+)
